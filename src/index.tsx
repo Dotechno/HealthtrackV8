0f24ce84f85e5c41b1e98c0385c332259684b2d4
Hayden Thai <thaihayden@gmail.com>
2023-02-19 19:08:09 -0800

    role authentication add on
    Added role attribute to authentication

--- src/index.tsx
+++ src/index.tsx
@@ -1,79 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import awsExports from './aws-exports';
 import './index.css';
-import * as AwsUI from '@awsui/components-react';
 import { Amplify } from 'aws-amplify';
-import { CheckboxField, SelectField, TextField, useAuthenticator } from '@aws-amplify/ui-react';
+import {
+  CheckboxField,
+  SelectField,
+  useAuthenticator,
+} from '@aws-amplify/ui-react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import App from './App';
 
 Amplify.configure(awsExports);
-
-//hi
-
-function App({ user }: { user: any }) {
-  const { signOut } = useAuthenticator((context) => [context.user]);
-  const [navigationOpen, setNavigationOpen] = React.useState(false);
-  const [isLoading, setLoading] = useState(false);
-
-
-
-  return (
-    <AwsUI.AppLayout
-      navigation={<></>}
-      content={
-        <AwsUI.ContentLayout
-          header={
-            <AwsUI.SpaceBetween size="m">
-              <AwsUI.Header
-                variant="h1"
-                info={<AwsUI.Link>Info</AwsUI.Link>}
-                description="This is a generic description used in the header."
-                actions={
-                  <AwsUI.Button onClick={signOut} variant="primary">
-                    Sign Out
-                  </AwsUI.Button>
-                }
-              >
-                Healthtrack
-              </AwsUI.Header>
-
-              <AwsUI.Alert>This is a generic alert.</AwsUI.Alert>
-            </AwsUI.SpaceBetween>
-          }
-        >
-          <AwsUI.Container
-            header={
-              <AwsUI.Header variant="h2" description="Container description">
-                Container header
-              </AwsUI.Header>
-            }
-          >
-            {`Your email is ${user.attributes.email}`}
-          </AwsUI.Container>
-        </AwsUI.ContentLayout>
-      }
-    />
-  );
-}
-
-const components = {
-
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Authenticator.Provider>
       <Authenticator
-        signUpAttributes={[
-          'email',
-
-          //   // Unwanted fields can be removed from the sign up form:
-          //   // 'gender',
-          //   // 'address',
-          //   // 'birthdate',
-        ]}
         components={{
           SignUp: {
             FormFields() {
@@ -81,18 +25,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
               return (
                 <>
-                  {/* https://ui.docs.amplify.aws/react/components/textfield */}
-                  <TextField
-                    name="username"
-                    descriptiveText="Please enter your username"
-                    label="Username"
-                    errorMessage={validationErrors.username as string}
-                  />
-
                   {/* Re-use default `Authenticator.SignUp.FormFields` */}
                   <Authenticator.SignUp.FormFields />
 
-                  <SelectField name="role" label="Roles">
+                  <SelectField name="custom:role" label="Roles">
                     <option value="Physician">Physician</option>
                     <option value="PhysicianAssisstant">Physician Assisstant</option>
                     <option value="Nurse">Nurse</option>
@@ -114,8 +50,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           }
         }}
       >
-        {({ user }) => <App user={user} />}
+        {({ user }): any => 
+         
+           <App user={user} />
+        }
       </Authenticator>
     </Authenticator.Provider>
-  </React.StrictMode >
+  </React.StrictMode>
 );
