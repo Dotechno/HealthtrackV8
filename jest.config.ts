a17efa33471a2ca91b383026b899d4cffb94d1e2
Hayden Thai <thaihayden@gmail.com>
2023-02-26 02:10:43 -0800

    configured Jest and wrote a unit test for the appointment picker

--- jest.config.ts
+++ jest.config.ts
@@ -11,8 +11,8 @@ export default {
   // when using React Testing Library and adds special
   // extended assertions to Jest
   setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect"
+    // "@testing-library/react/cleanup-after-each",
+    // "@testing-library/jest-dom/extend-expect"
   ],
 
   // Test spec file resolution pattern
